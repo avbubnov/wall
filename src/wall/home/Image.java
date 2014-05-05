d051ca3a4bf9a79cb481805c3c45dd9f090d8656
avbubnov <avbubnov@mail.ru>
2014-05-05 02:58:11 -0700

    commit_04

--- src/wall/home/Image.java
+++ src/wall/home/Image.java
@@ -4,9 +4,9 @@ public class Image {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Image:main:01+");
+		System.out.println("Image:main:01");
 		
-		System.out.println("Image:main:02");
+		System.out.println("Image:main:02+");
 	}
 
 }
