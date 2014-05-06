75cd29509f10746abd78159e28b28e41a9ff593f
bubnov <bubnov@35-BUBNOV.fire.iac.spb.ru>
2014-05-06 16:25:38 +0400

    commit -13-

--- src/wall/home/Image.java
+++ src/wall/home/Image.java
@@ -4,14 +4,14 @@ public class Image {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Image:main:01++");
+		System.out.println("Image:main:001");
 		
 	}
 	
 
 	public void search(String name) {
 
-            System.out.println("Image:search:01");
+            System.out.println("Image:search:001");
 
-         }
+   }
 }
